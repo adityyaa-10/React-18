a977fa19bc44ae1f72611db1d09e38d34a27f492
Aditya <aditya.pandey.241103@gmail.com>
2023-04-30 13:01:45 +0530

    Event Handling in React 18

--- Bookseller Project/src/index.js
+++ Bookseller Project/src/index.js
@@ -1,49 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { firstBook, secondBook, thirdBook, fourthBook, fifthBook, sixthBook } from './Books'
-
-
-//  const firstBook = {
-//   image: "https://m.media-amazon.com/images/I/51eDMGwZVvL._SX330_BO1,204,203,200_.jpg",
-//   title: "I Will Teach You to Be Rich",
-//   author: "Ramit Sethi",
-// }
-//  const secondBook = {
-//   image: "https://m.media-amazon.com/images/I/41T-XHe8-EL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-//   title: "The Body Keeps the Score",
-//   author: "Bessel van der Kolk M.D.",
-// }
-//  const thirdBook = {
-//   image: "https://m.media-amazon.com/images/I/41N62t+ANTL._SX331_BO1,204,203,200_.jpg",
-//   title: "The Mountain Is You",
-//   author: "Brianna Wiest",
-// }
-//  const fourthBook = {
-//   image: "https://m.media-amazon.com/images/I/51QZyQzA83L._SX327_BO1,204,203,200_.jpg",
-//   title: "The Wager",
-//   author: "David Grann",
-// }
-//  const fifthBook = {
-//   image: "https://m.media-amazon.com/images/I/41ZLnc34EiL._SX332_BO1,204,203,200_.jpg",
-//   title: "Verity",
-//   author: "Colleen Hoover",
-// }
-//  const sixthBook = {
-//   image: "https://m.media-amazon.com/images/I/511zwchU0oL._SX327_BO1,204,203,200_.jpg",
-//   title: "You Can't Joke About That",
-//   author: "Kat Timpf"
-// }
+import { booksdetails } from './Books'
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book image={firstBook.image} title={firstBook.title} author={firstBook.author} />
-      <Book image={secondBook.image} title={secondBook.title} author={secondBook.author} />
-      <Book image={thirdBook.image} title={thirdBook.title} author={thirdBook.author} />
-      <Book image={fourthBook.image} title={fourthBook.title} author={fourthBook.author} />
-      <Book image={fifthBook.image} title={fifthBook.title} author={fifthBook.author} />
-      <Book image={sixthBook.image} title={sixthBook.title} author={sixthBook.author} />
+      <EventExamples />
+      {booksdetails.map((book) => {
+        return <div>
+          <Book {...book} key={booksdetails.id} />
+        </div>
+      })}
+    </section>
+  );
+}
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('Handle Form Input');
+  };
+  const handleButtonClick = () => {
+    alert('Handle Button Click')
+  };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log('Form Submitted Successfully')
+  };
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button type='submit' onClick={handleButtonClick}>Click Me</button>
     </section>
   );
 }
